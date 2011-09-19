b76b680c8b7c329f2f884ed51d2805d5171b7b04
Eric Wu <searchsignals@gmail.com>
2011-09-19 16:38:00 -0700

    Punctionation stripping and replacement methods.

--- lib/stringfu.rb
+++ lib/stringfu.rb
@@ -1,5 +1,31 @@
 require "stringfu/version"
 
+class String
+  include Stringfu
+end
+
 module Stringfu
-  # Your code goes here...
+    def punc_strip
+      self.gsub(/[!@#\$%^&*;:,<.>?\/|+=\[\]"{}()_-]/,'').gsub(/^'/,'')
+    end
+
+    def punc_strip!
+      replace(self.gsub(/[!@#\$%^&*;:,<.>?\/|+=\[\]"{}()_-]/,'').gsub(/^'/,''))
+    end
+
+    def punc_sub pattern=' '
+      self.sub(/[!@#\$%^&*;:,<.>?\/|+=\[\]"{}()_-]/,pattern).sub(/^'/,pattern)
+    end
+
+    def punc_sub! pattern=' '
+      replace(self.sub(/[!@#\$%^&*;:,<.>?\/|+=\[\]"{}()_-]/,pattern).sub(/^'/,pattern))
+    end
+
+    def punc_gsub pattern=' '
+      self.gsub(/[!@#\$%^&*;:,<.>?\/|+=\[\]"{}()_-]/,pattern).gsub(/^'/,pattern)
+    end
+
+    def punc_gsub! pattern=' '
+      replace(self.gsub(/[!@#\$%^&*;:,<.>?\/|+=\[\]"{}()_-]/,pattern).gsub(/^'/,pattern))
+    end
 end
