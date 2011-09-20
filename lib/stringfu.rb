2937a0b2714325d5fc8fe82406be0d3cd2dfac26
Eric Wu <searchsignals@gmail.com>
2011-09-19 21:21:57 -0700

    Appended UEAStemmer functionality to allow easy stemming of words."

--- lib/stringfu.rb
+++ lib/stringfu.rb
@@ -1,4 +1,8 @@
-require "stringfu/version"
+require 'rubygems'
+require 'bundler/setup'
+
+require 'stringfu/version'
+require 'uea-stemmer'
 
 STOP = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"]
 
@@ -58,6 +62,23 @@ module Stringfu
       self.class.__send__(:alias_method, "_#{num}grams=", "#{prefix}grams=") if num <= 3
     end
     args
+  end
+
+  def stem
+    words = self.punc_strip.normalize.split
+
+    stemmer = UEAStemmer.new
+    words = words.map do |word|
+      stem = stemmer.stem word
+    end
+  end
+
+  def normalize
+    self.downcase.squeeze(" ").strip
+  end
+
+  def normalize!
+    replace(self.downcase.squeeze(" ").strip)
   end
 
   private
